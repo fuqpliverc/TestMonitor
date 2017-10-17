bd28aa2fde5cb4079e3898f6f1453a14c06c55d3
fuqpliverc <sdkjflksdhdhkd@sina.com>
2017-10-17 16:18:54 +0800

    test

--- MyMonitor/src/com/rcplatformhk/monitor/MonitorController.java
+++ MyMonitor/src/com/rcplatformhk/monitor/MonitorController.java
@@ -1,7 +1,5 @@
 package com.rcplatformhk.monitor;
-@RestController
-@Api(value = "monotor", description = "test")
-@RequestMapping("monitor")
+
 public class MonitorController {
 
     public static String flag = "monitor";
