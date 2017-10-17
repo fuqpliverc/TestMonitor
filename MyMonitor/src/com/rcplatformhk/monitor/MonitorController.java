7666c12e3ee349133be9b5f644fbb38be546a4f9
fuqpliverc <32133950+fuqpliverc@users.noreply.github.com>
2017-10-17 16:20:16 +0800

    Update MonitorController.java

--- MyMonitor/src/com/rcplatformhk/monitor/MonitorController.java
+++ MyMonitor/src/com/rcplatformhk/monitor/MonitorController.java
@@ -3,7 +3,7 @@ package com.rcplatformhk.monitor;
 public class MonitorController {
 
     public static String flag = "monitor";
-
+    public static String system_monitor = "system_monitor";
 
 
 }
